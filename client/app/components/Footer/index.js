89e7bd95ee3699e1a28c0d7dc296dff2b8714758
git-palace <edeveloper4@gmail.com>
2018-11-14 13:54:52 -0800

    update

--- client/app/components/Footer/index.js
+++ client/app/components/Footer/index.js
@@ -6,14 +6,14 @@ const Footer = () => (
   <footer className="w-100 row mx-auto px-5 py-4">
     <Link to="/help" className="text-uppercase">Help</Link>
 
-    <a className="ml-auto mb-0">Download our mobile apps</a>
+    <a className="ml-auto mb-0 mr-2">Download our mobile apps</a>
 
-    <Link to="" className="text-uppercase ml-4 mr-2">iPhone</Link>
-    <Link to="" className="text-uppercase mx-2">Android</Link>
-    <Link to="" className="social-icon mx-2"><span><i className="fa fa-facebook" aria-hidden="true"></i></span></Link>
-    <Link to="" className="social-icon mx-2"><span><i className="fa fa-twitter" aria-hidden="true"></i></span></Link>
-    <Link to="" className="social-icon mx-2"><span><i className="fa fa-instagram" aria-hidden="true"></i></span></Link>
-    <Link to="" className="social-icon ml-2"><span><i className="fa fa-linkedin-square" aria-hidden="true"></i></span></Link>
+    <a href="https://store.google.com/#" className="text-uppercase ml-4 mr-2">iPhone</a>
+    <a href="https://store.google.com/#" className="text-uppercase mx-2">Android</a>
+    <a href="https://facebook.com/#" className="social-icon mx-2"><span><i className="fa fa-facebook" aria-hidden="true"></i></span></a>
+    <a href="https://twitter.com/#" className="social-icon mx-2"><span><i className="fa fa-twitter" aria-hidden="true"></i></span></a>
+    <a href="https://instagram.com/#" className="social-icon mx-2"><span><i className="fa fa-instagram" aria-hidden="true"></i></span></a>
+    <a href="https://linkedin.com/#" className="social-icon ml-2"><span><i className="fa fa-linkedin-square" aria-hidden="true"></i></span></a>
   </footer>
 );
 
